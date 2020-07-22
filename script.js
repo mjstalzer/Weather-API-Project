a93da9752b174a2fd4527ffea72c285b117a500d
Matt Stalzer <mjs58181@uga.edu>
2020-07-22 16:44:55 -0400

    functional but no local storage

--- script.js
+++ script.js
@@ -1,54 +1,8 @@
-function empty() {
-    $(".weather").empty()
-}
-
-
-
-
-
-$(".list-group-item").on("click", function () {
-
-    var x = $(this).data("search")
-
-    console.log(x)
-
-    var apiKey = "0659bcfbc663939009bde73b4afdb4fc"
-
-    var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + x + "&appid=" + apiKey
-
-    $.ajax({
-        "url": queryURL,
-        "method": "GET"
-    }).then(function (response) {
-
-        // <-------------------------------> \\ 
-        var kelvin = (response.main.temp)
-        var fahrenheit = (kelvin - 273.15) * 1.80 + 32
-        // roundedF is a string !!! \\ 
-        var roundedF = fahrenheit.toFixed(2)
-        console.log("Temperature: " + roundedF)
-        // <--------------------------------> \\ 
-        // humidity response is a number \\
-        var humidity = (response.main.humidity)
-        // turn response into string \\
-        var humidString = humidity.toString();
-        console.log("Humidity: " + humidString + "%")
-        // <--------------------------------> \\
-        var speed = response.wind.speed
-        var windSpeed = speed.toString();
-        console.log("Wind Speed: " + windSpeed + " MPH")
-        // <---------------------------------> \\
-        // console.log(response)
-
-    });
-})
-
 
 $(".searchBtn").on("click", function () {
+    event.preventDefault();
 
     $("#resultsCard").empty();
-    // $(".row").empty();
-    // $("h3").empty();
 
     var newCity = $("input").val().trim();
     var cityAPI = (newCity.charAt(0).toUpperCase() + newCity.slice(1))
@@ -79,7 +33,7 @@ $(".searchBtn").on("click", function () {
             "method": "GET"
         }).then(function (response) {
 
-            console.log(response)
+            // console.log(response)
             // <-------------------------------> \\ 
             var kelvin = (response.main.temp)
             var fahrenheit = (kelvin - 273.15) * 1.80 + 32
@@ -100,36 +54,39 @@ $(".searchBtn").on("click", function () {
             var windP = $("<p>")
             windP.text("Wind Speed: " + speed + " MPH")
             var date = moment().format("L");
-            var cityH1 = $("<h2>").text(cityAPI + " (" + date + ")")
+            var cityH1 = $("<h3>").text(cityAPI + " (" + date + ")")
+            var weatherIcon = $("<img>").addClass("weather-icon")
+            var weatherImg = response.weather[0].icon
+            weatherIcon.attr("src", "http://openweathermap.org/img/wn/" + weatherImg + ".png")
+            cityH1.append(weatherIcon)
             // <---------------------------------> \\
 
             var lat = response.coord.lat
             var lon = response.coord.lon
 
             $.ajax({
-                "url" : "http://api.openweathermap.org/data/2.5/uvi/forecast?appid=" + apiKey + "&lat=" + lat + "&lon=" + lon ,
-                "method" : "GET"
-            }).then(function(UV){
+                "url": "http://api.openweathermap.org/data/2.5/uvi/forecast?appid=" + apiKey + "&lat=" + lat + "&lon=" + lon,
+                "method": "GET"
+            }).then(function (UV) {
 
                 var uv = UV[0].value
-                console.log(uv)
+                // console.log(uv)
 
                 var uvP = $("<p>")
-                uvP.text("UV Index: " + uv)
-                if (uv < 5){
-                    console.log("less than five")
-                    uvP.addClass("green")
+                var uvB = $("<button>")
+                uvP.text("UV Index: ")
+                uvB.text(uv)
+                if (uv < 5) {
+                    uvB.addClass("btn btn-success")
                 }
                 if (uv > 5) {
-                    console.log("greater than 5")
-                    uvP.addClass("red")
+                    uvB.addClass("btn btn-danger")
                 }
                 if (uv === 5) {
-                    console.log("is 5")
-                    uvP.addClass("yellow")
+                    uvB.addClass("btn btn-warning")
                 }
                 $("#resultsCard").append(cityH1, tempP, humidP, windP, uvP)
-
+                $(uvP).append(uvB)
             })
 
         });
@@ -140,49 +97,42 @@ $(".searchBtn").on("click", function () {
             "method": "GET"
         }).then(function (resp2) {
 
-            // console.log(resp2)
-
-            // var day1 = ;   
-            // console.log(day1)
-            var x = resp2.list[0].main.temp
-            dayOneT = ((x - 273.15) * 1.80 + 32).toFixed(2);
-            var dayOneH = resp2.list[0].main.humidity
-            var oneT = $("<p>").text("Temp: " + dayOneT + " °F")
-            var oneH = $("<p>").text("Humidity: " + dayOneH + "%")
-            $("#day1").append(oneT, oneH)
-
-            var y = resp2.list[8].main.temp
-            dayTwoT = ((y - 273.15) * 1.80 + 32).toFixed(2);
-            var dayTwoH = resp2.list[8].main.humidity
-            var twoT = $("<p>").text("Temp: " + dayTwoT + " °F")
-            var twoH = $("<p>").text("Humidity: " + dayTwoH + "%")
-            $("#day2").append(twoT, twoH)
-
-            var z = resp2.list[16].main.temp
-            dayThreeT = ((z - 273.15) * 1.80 + 32).toFixed(2);
-            var dayThreeH = resp2.list[16].main.humidity
-            var threeT = $("<p>").text("Temp: " + dayThreeT + " °F")
-            var threeH = $("<p>").text("Humidity: " + dayThreeH + "%")
-            $("#day3").append(threeT, threeH)
-
-            moment().add(1, 'days').calendar();
-            var t = resp2.list[24].main.temp
-            dayFourT = ((t - 273.15) * 1.80 + 32).toFixed(2);
-            var dayFourH = resp2.list[24].main.humidity
-            var fourT = $("<p>").text("Temp: " + dayFourT + " °F")
-            var fourH = $("<p>").text("Humidity: " + dayFourH + "%")
-            $("#day4").append(fourT, fourH)
-
-            var r = resp2.list[32].main.temp
-            dayFiveT = ((r - 273.15) * 1.80 + 32).toFixed(2);
-            var dayFiveH = resp2.list[32].main.humidity
-            var fiveT = $("<p>").text("Temp: " + dayFiveT + " °F")
-            var fiveH = $("<p>").text("Humidity: " + dayFiveH + "%")
-            $("#day5").append(fiveT, fiveH)
-
-
+            $("#break").text("5-Day Forecast:")
+            $(".col-lg-2").remove();
+            console.log(resp2)
+            for (var i = 0; i < resp2.list.length; i++) {
+                if (resp2.list[i].dt_txt.includes("12:00:00")) {
+                    console.log([i])
+                    var icon = resp2.list[i].weather[0].icon
+                    var x = resp2.list[i].main.temp
+                    var temp = ((x - 273.15) * 1.80 + 32).toFixed(2);
+                    var hum = resp2.list[i].main.humidity
+                    console.log(temp, hum, icon)
+                    var div1 = $("<div>").addClass("col-lg-2 card text-white bg-primary mb-3").attr("style" , "width: 12rem;").attr("id" , "forecast5").css("padding" , "10px")
+                    $("#fiveDay").append(div1)
+                    var tempP = $("<p>").text("Temp: " + temp + " °F")
+                    var humiP = $("<p>").text("Humidity: " + hum + "%")
+                    var weatherIcon2 = $("<img>").addClass("weather-icon").height(50).width(50)
+                    weatherIcon2.attr("src", "http://openweathermap.org/img/wn/" + icon + ".png")
+                    var date = (resp2.list[i].dt_txt).slice(0,10)
+                    var dateP = $("<p>").text(date)
+                    div1.append(dateP, weatherIcon2, tempP , humiP)
+                }
+            }
         });
 
     }
 })
+
+$(".searchBtn").on("click", function () {
+    event.preventDefault();
+    // alert("local storage")
+    var store = $(this).siblings().val()
+    var stored = store.charAt(0).toUpperCase() + store.slice(1)
+
+    localStorage.setItem(stored, stored)
+    // console.log(stored)
+
+})
+
 
